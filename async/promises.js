c20fa37c87bd052e89ca5f543987a7c0430c5ae2
Fabricio Samanez <fabricio.samanez@gmail.com>
2021-05-31 11:51:48 -0500

    se agregan promesas

---
+++ async/promises.js
@@ -0,0 +1,48 @@
+function hola(nombre){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+        console.log('Hola, '+nombre);
+        resolve(nombre);
+        },1500);
+    });
+   
+}
+
+function hablar(nombre) {
+    return new Promise((resolve,reject) => {
+        setTimeout(function(){
+            console.log('bla bla bla bla...');
+            resolve(nombre);
+        }, 1000)
+    })
+    
+}
+
+function adios(nombre){
+    return new Promise((resolve,reject) =>{
+        setTimeout(function(){
+        console.log('Adios', nombre)
+        resolve();
+        },1000);
+    });
+    
+}
+
+// ---
+
+console.log('Iniciando el proceso')
+hola('Fabricio')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('Terminado el proceso')
+    })
+    .catch(error =>
+    {
+        //siempre utilizar catch;
+        console.error('Ha habido un error:');
+        console.error(error);
+    })
+   

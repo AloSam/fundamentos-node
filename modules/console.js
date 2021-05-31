bfac78c020dad4b036bd4dba7cfa16b340f47975
Fabricio Samanez <fabricio.samanez@gmail.com>
2021-05-31 16:18:31 -0500

    se agrega console

---
+++ modules/console.js
@@ -0,0 +1,56 @@
+// console.log('algo'); //mostrar en consola
+// console.info('algo'); //usado para informar
+// console.error('algo'); //usado para mostrar error
+// console.warn('algo'); //usado para mostrar warning
+
+// var tabla = [
+//     {
+//         a: 1,
+//         b: 'Z'
+//     },
+//     {
+//         a: 2,
+//         b: 'Y'
+//     }
+// ]
+// console.log(tabla);
+// console.table(tabla); // para mostrar una tabla
+// console.group('Conversación');
+// console.log('Hola');
+// console.group('bla');
+// console.log('Blablabla');
+// console.log('Blablabla');
+// console.log('Blablabla');
+// console.groupEnd('bla');
+// console.log('Adios');
+// console.groupEnd('Conversación');
+
+// console.log ('otra cosas de otra funcion');
+
+// function function1(){
+//     console.group('funcion 1'); // para agrupar logs
+//     console.log('Esto es de la funcion 1');
+//     console.log('Esto tambien es de la funcion 1');
+//     function2();
+//     console.log('Volvemos a funcion 1');
+//     console.groupEnd('funcion 1');
+// }
+
+// function function2(){
+//     console.group('funcion 2');
+//     console.log('ahora estamos en la funcion 2');
+//     console.groupEnd('funcion 2');
+// }
+
+// console.log('empezamos');
+// function1();
+
+// console.count('veces'); // para contar
+// console.count('veces');
+// console.count('veces');
+// console.countReset('veces');
+// console.count('veces');
+
+
+
+

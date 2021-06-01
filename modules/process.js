ac3e764e7ab284c1bcfafd983657d9f7ecde8110
Fabricio Samanez <fabricio.samanez@gmail.com>
2021-06-01 15:15:24 -0500

    ejemplos process

---
+++ modules/process.js
@@ -0,0 +1,25 @@
+
+process.on('beforeExit', () =>{
+    console.log('el proceso va a terminar');
+});
+
+
+
+process.on('exit', () =>{
+    console.log('Ale, el proceso acabó');
+    setTimeout(() =>{
+        console.log('Esto no se va a ver nunca');
+    },0)
+});
+
+setTimeout(() =>{
+    console.log('Esto se va a ver');
+},0)
+
+process.on('uncaughtException', (err,origin) => {
+    console.error('Vaya, se nos ha olvidado capturar un error');
+    console.error(err);
+}) // para capturar errores
+
+funcionQueNoexiste();
+process.on('unhandleRejection') //para promesas
